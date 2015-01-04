fc3943f5037e20f75f7730da6672256c05b1f99c
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-01-04 14:09:41 +0100

    Added ignore file and also made the necessary changes for compatibility with new OS installation.

--- RealmPlayersAddons/CopyToWowDir.bat
+++ RealmPlayersAddons/CopyToWowDir.bat
@@ -1,2 +1,2 @@
-xcopy /s /D /Y "..\TBC" "D:\Program\WoW TBC\Interface\AddOns"
-xcopy /s /D /Y "..\Classic" "D:\Program\World Of Warcraft Classic\Interface\AddOns"
+xcopy /s /D /Y "..\TBC" "D:\Program Files (x86)\World Of Warcraft TBC\Interface\AddOns"
+xcopy /s /D /Y "..\Classic" "D:\Program Files (x86)\World Of Warcraft Classic\Interface\AddOns"
