f8a92c8eee2ef14407286fd19054808040c35d15
Viktor Friberg <ViktorFriberg89@gmail.com>
2015-04-20 21:48:40 +0200

    Support for bags, bank items and keyring

--- Classic/VF_CharacterStats/VF_CharacterStats.lua
+++ Classic/VF_CharacterStats/VF_CharacterStats.lua
@@ -4,12 +4,21 @@ VF_CharacterStatsVersion = VF_CHARACTERSTATSVERSION;
 
 if string.find(GetBuildInfo(), "^1%.") then
 
+VF_ThisRealm = "";
+VF_ThisPlayer = "";
+VF_ThisData = nil;
+
 function VF_CharacterStats_Message(_Text)
 	DEFAULT_CHAT_FRAME:AddMessage(_Text);
 end
 
 function VF_CharacterStats_OnLoad()
 	this:RegisterEvent("VARIABLES_LOADED");
+	this:RegisterEvent("BAG_UPDATE");
+	this:RegisterEvent("BANKFRAME_OPENED");
+	this:RegisterEvent("PLAYERBANKSLOTS_CHANGED");
+	this:RegisterEvent("PLAYERBANKBAGSLOTS_CHANGED");
+	this:RegisterEvent("PLAYER_MONEY");
 	SlashCmdList["CHARACTERSTATS"] = VF_CharacterStats_Command;
 	SLASH_CHARACTERSTATS1 = "/VFCS";
 	SLASH_CHARACTERSTATS2 = "/CharacterStats";
@@ -21,7 +30,101 @@ function VF_CharacterStats_OnEvent()
 			VF_CharacterStatsData = {};
 		end
 		VF_CharacterStatsData.Version = VF_CHARACTERSTATSVERSION;
+		VF_ThisRealm = GetRealmName();
+		VF_ThisPlayer = UnitName("player");
+		if(VF_CharacterStatsData[VF_ThisRealm] == nil) then
+			VF_CharacterStatsData[VF_ThisRealm] = {};
+		end
+		if(VF_CharacterStatsData[VF_ThisRealm][VF_ThisPlayer] == nil) then
+			VF_CharacterStatsData[VF_ThisRealm][VF_ThisPlayer] = {};
+		end
+		VF_ThisData = VF_CharacterStatsData[VF_ThisRealm][VF_ThisPlayer];
+		VF_ThisData.V = VF_CharacterStatsData.Version;
+		
 		DEFAULT_CHAT_FRAME:AddMessage("VF_CharacterStats(/CharacterStats) version "..VF_CharacterStatsVersion.." loaded!");
+	elseif (event == "BAG_UPDATE" or event == "BANKFRAME_OPENED" or event == "PLAYERBANKSLOTS_CHANGED" or event == "PLAYERBANKBAGSLOTS_CHANGED") then
+		VF_CharacterStats_SaveItems();
+		VF_CharacterStats_SaveMoney();
+	elseif (event == "PLAYER_MONEY") then
+		VF_CharacterStats_SaveMoney();
+	elseif (event == "MAIL_SEND_SUCCESS") then
+		VF_CharacterStats_SaveSentMail();
+	elseif(event == "MAIL_INBOX_UPDATE") then
+		VF_CharacterStats_SaveMailInbox();
+	end
+end
+
+function VF_GetPrecision_2(_Value)
+	return math.floor(_Value * 100) / 100;
+end
+
+function VF_CharacterStats_SaveMoney()
+	VF_ThisData["M"] = GetMoney();
+end
+
+function VF_CharacterStats_SaveItems()
+	if(BankFrame:IsVisible()) then
+		if (BankFrame:IsVisible()) then
+			VF_ThisData.BI = "";
+			for num = 1, 24 do
+				local itemLink = GetContainerItemLink(BANK_CONTAINER, num);
+				local icon, quantity = GetContainerItemInfo(BANK_CONTAINER, num);
+				if ( itemLink ) then
+					local _, _, rubbish1, item, rubbish2 = string.find(itemLink, "(.*)|Hitem:(.*)|h%[(.*)");
+					VF_ThisData.BI = VF_ThisData.BI..";"..quantity.."x"..item;
+				else
+					VF_ThisData.BI = VF_ThisData.BI..";";
+				end
+			end
+		end
+	end
+	
+	VF_ThisData.KR = "";
+	for num = 1, 32 do
+		local itemLink = GetContainerItemLink(KEYRING_CONTAINER, num);
+		local icon, quantity = GetContainerItemInfo(KEYRING_CONTAINER, num);
+		if ( itemLink ) then
+			local _, _, rubbish1, item, rubbish2 = string.find(itemLink, "(.*)|Hitem:(.*)|h%[(.*)");
+			VF_ThisData.KR = VF_ThisData.KR..";"..quantity.."x"..item;
+		else
+			VF_ThisData.KR = VF_ThisData.KR..";";
+		end
+	end
+
+	for bagIndex = 0, 10 do
+		if(bagIndex < 5 or BankFrame:IsVisible()) then
+			local bagItemLink = nil;
+			if(bagIndex < 5) then
+				local bagID = ContainerIDToInventoryID(bagIndex)
+				bagItemLink = GetInventoryItemLink("player", bagID);
+			else
+				local bagID = BankButtonIDToInvSlotID(bagIndex, 1);
+				bagItemLink = GetInventoryItemLink("player", bagID);
+			end
+			
+			if (bagItemLink or bagIndex == 0 or bagIndex == 11) then
+				local thisBag = "";
+				if(bagIndex ~= 0) then
+					local _, _, rubbish1, bagItem, rubbish2 = string.find(bagItemLink, "(.*)|Hitem:(.*)|h%[(.*)");
+					thisBag = ""..bagItem;
+				end
+					
+				bagSlots = GetContainerNumSlots(bagIndex);
+				for bagItem = 1, bagSlots do
+					local itemLink = GetContainerItemLink(bagIndex, bagItem);
+					local icon, quantity = GetContainerItemInfo(bagIndex, bagItem);
+					if ( itemLink ) then
+						local _, _, rubbish1, item, rubbish2 = string.find(itemLink, "(.*)|Hitem:(.*)|h%[(.*)");
+						thisBag = thisBag..";"..quantity.."x"..item;
+					else
+						thisBag = thisBag..";";
+					end
+				end
+				VF_ThisData["B"..bagIndex] = thisBag;
+			else
+				VF_ThisData["B"..bagIndex] = nil;
+			end
+		end
 	end
 end
 
