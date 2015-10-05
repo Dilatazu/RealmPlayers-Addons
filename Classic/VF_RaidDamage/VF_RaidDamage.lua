f3e950629fc1b24c441e2b9da14b86f56aff6996
Dilatazu <Dilatazu@gmail.com>
2015-10-05 22:13:13 +0200

    Fixed Nostalrius Compatibility with Vaelastrasz
    Bossyell was not correctly tuned for Nostalrius.

--- Classic/VF_RaidDamage/VF_RaidDamage.lua
+++ Classic/VF_RaidDamage/VF_RaidDamage.lua
@@ -858,19 +858,19 @@ VF_RD_YellEventsDE = {
 VF_RD_YellEventsFR = {
 	--MC
 	["Majordomo Executus"] = {
-		["Imprudents mortels, nul ne défie les fils de la Flamme Vivante !"] = VF_RD_YellEvents_Start, --TODO fix utf8
+		["Imprudents mortels, nul ne dÃ©fie les fils de la Flamme Vivante !"] = VF_RD_YellEvents_Start, --TODO fix utf8
 		["Impossible ! Retenez vos coups, mortels ! Je me rends !"] = VF_RD_YellEvents_End,
 	},
 	["Ragnaros"] = {
-		["À VOTRE TOUR INSECTES ! VOUS VOULIEZ CONNAÎTRE LE POUVOIR DE RAGNAROS. VOUS ALLEZ LE DÉCOUVRIR !"] = VF_RD_YellEvents_Start, --TODO fix utf8
-		["VENEZ ! Ô MES SERVITEURS ! VENEZ DÉFENDRE VOTRE MAÎTRE !"] = VF_RD_YellEvents_Phase2, --TODO fix utf8
+		["Ã€ VOTRE TOUR INSECTES ! VOUS VOULIEZ CONNAÃŽTRE LE POUVOIR DE RAGNAROS. VOUS ALLEZ LE DÃ‰COUVRIR !"] = VF_RD_YellEvents_Start, --TODO fix utf8
+		["VENEZ ! Ã” MES SERVITEURS ! VENEZ DÃ‰FENDRE VOTRE MAÃŽTRE !"] = VF_RD_YellEvents_Phase2, --TODO fix utf8
 	},
 	
 	--Onyxia
 	["Onyxia"] = {
 		["Quelle chance. D'habitude je dois quitter mon antre pour me nourrir."] = VF_RD_YellEvents_Start,
-		["Ces efforts inutiles m'ennuient. Je vais vous incinérer depuis les airs."] = VF_RD_YellEvents_Phase2, --TODO fix utf8
-		["On dirait que vous avez besoin d'une nouvelle leçon, mortels !"] = VF_RD_YellEvents_Phase3, --TODO fix utf8
+		["Ces efforts inutiles m'ennuient. Je vais vous incinÃ©rer depuis les airs."] = VF_RD_YellEvents_Phase2, --TODO fix utf8
+		["On dirait que vous avez besoin d'une nouvelle leÃ§on, mortels !"] = VF_RD_YellEvents_Phase3, --TODO fix utf8
 	},
 	
 	--ZG
@@ -881,27 +881,27 @@ VF_RD_YellEventsFR = {
 	},
 	["High Priest Venoxis"] = {
 		["Que se d\195\169roulent les anneaux de la haine"] = VF_RD_YellEvents_Phase2,
-		["La sérénité... enfin !"] = VF_RD_YellEvents_End, --TODO fix utf8
+		["La sÃ©rÃ©nitÃ©... enfin !"] = VF_RD_YellEvents_End, --TODO fix utf8
 	},
 	["High Priestess Mar'li"] = {
-		["Entraine moi dans ta toile dame Shadra ! Déchaine ton venin !"] = VF_RD_YellEvents_Start, --TODO fix utf8
-		["Soyez béni mortel pour m'avoir libérée. Hakkar ne me contrôle plus."] = VF_RD_YellEvents_End, --TODO fix utf8
+		["Entraine moi dans ta toile dame Shadra ! DÃ©chaine ton venin !"] = VF_RD_YellEvents_Start, --TODO fix utf8
+		["Soyez bÃ©ni mortel pour m'avoir libÃ©rÃ©e. Hakkar ne me contrÃ´le plus."] = VF_RD_YellEvents_End, --TODO fix utf8
 	},
 	["Bloodlord Mandokir"] = {
-		["Je jetterai vos âmes en patûre à Hakkar lui-même !"] = VF_RD_YellEvents_Start, --TODO fix utf8
+		["Je jetterai vos Ã¢mes en patÃ»re Ã  Hakkar lui-mÃªme !"] = VF_RD_YellEvents_Start, --TODO fix utf8
 	},
 	["High Priest Thekal"] = {
 		["Shirvallah ! Que ta rage m'envahisse !"] = VF_RD_YellEvents_Start,
 		["Hakkar ne me domine plus. Je connais enfin la paix !"] = VF_RD_YellEvents_End,
 	},
 	["High Priestess Arlokk"] = {
-		["Bethekk, ta prêtresse en appelle à ta puissance !"] = VF_RD_YellEvents_Start, --TODO fix utf8
-		["Enfin ! Je suis enfin libérée de l'Ecorcheur d'âme !"] = VF_RD_YellEvents_End, --TODO fix utf8
+		["Bethekk, ta prÃªtresse en appelle Ã  ta puissance !"] = VF_RD_YellEvents_Start, --TODO fix utf8
+		["Enfin ! Je suis enfin libÃ©rÃ©e de l'Ecorcheur d'Ã¢me !"] = VF_RD_YellEvents_End, --TODO fix utf8
 	},
 	["Jin'do the Hexxer"] = {
 	},
 	["Hakkar"] = {
-		["L'ORGUEIL ANNONCE LA FIN DE VOTRE MONDE. AVANCEZ MORTELS ! AFFRONTEZ LA COLÈRE DE L'ECORCHEUR D'ÂME !"] = VF_RD_YellEvents_Start, --TODO fix utf8
+		["L'ORGUEIL ANNONCE LA FIN DE VOTRE MONDE. AVANCEZ MORTELS ! AFFRONTEZ LA COLÃˆRE DE L'ECORCHEUR D'Ã‚ME !"] = VF_RD_YellEvents_Start, --TODO fix utf8
 	},
 	
 	--BWL
@@ -912,10 +912,10 @@ VF_RD_YellEventsFR = {
 		["La chambre des oeufs est envahie !"] = VF_RD_YellEvents_Start, --only here for HaveStartYell compatibility
 	},
 	["Vaelastrasz the Corrupt"] = {
-		["DESTRUCTION ! Tremblez mortels, devant la colère du seigneur... Non ! Je dois lutter !"] = VF_RD_YellEvents_Start,--TODO fix utf8
+		["DESTRUCTION ! Tremblez mortels, devant la colÃ¨re du seigneur... Non ! Je dois lutter !"] = VF_RD_YellEvents_Start,--TODO fix utf8
 	},
 	["Broodlord Lashlayer"] = {
-		["Aucun membre de votre espèce ne devrait être ici. Vous vous êtes condamnés vous mêmes !"] = VF_RD_YellEvents_Start,--TODO fix utf8
+		["Aucun membre de votre espÃ¨ce ne devrait Ãªtre ici. Vous vous Ãªtes condamnÃ©s vous mÃªmes !"] = VF_RD_YellEvents_Start,--TODO fix utf8
 	},
 	["Lord Victor Nefarius"] = {--Nefarian
 	},
@@ -947,7 +947,7 @@ VF_RD_YellEventsFR = {
 	},
 	["Thane Korth'azz"] = {
 		["Allez viens te battre, mauviette !"] = VF_RD_YellEvents_Start,
-		["Quel gâchis de sang !"] = VF_RD_YellEvents_End, --TODO fix utf8
+		["Quel gÃ¢chis de sang !"] = VF_RD_YellEvents_End, --TODO fix utf8
 	},
 	["Lady Blaumeux"] = {
 	},
@@ -1025,6 +1025,7 @@ VF_RD_YellEvents = {
 	},
 	["Vaelastrasz the Corrupt"] = {
 		["mortals before the wrath of Lord"] = VF_RD_YellEvents_Start,
+		["COWER MORTALS BEFORE THE WRATH OF LORD"] = VF_RD_YellEvents_Start,--For Nostalrius Compatibility
 	},
 	["Broodlord Lashlayer"] = {
 		["None of your kind should be here"] = VF_RD_YellEvents_Start,
